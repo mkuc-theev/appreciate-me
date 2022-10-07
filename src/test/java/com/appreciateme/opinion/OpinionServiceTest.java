091e09a2d5e316063b767c47e7b0df64e2fd39d0
atyranski <atyranski@griddynamics.com>
2022-10-07 16:23:03 +0200

    Testcontainers first attempt

--- src/test/java/com/appreciateme/opinion/OpinionServiceTest.java
+++ src/test/java/com/appreciateme/opinion/OpinionServiceTest.java
@@ -2,6 +2,7 @@ package com.appreciateme.opinion;
 
 import com.appreciateme.opinion.dto.OpinionDTO;
 import com.appreciateme.opinion.dto.OpinionMapper;
+import com.appreciateme.opinion.exception.OpinionNotFoundException;
 import com.appreciateme.opinion.model.Opinion;
 import com.appreciateme.opinion.repository.OpinionRepository;
 import com.appreciateme.opinion.service.OpinionService;
@@ -10,8 +11,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -69,4 +73,71 @@ public class OpinionServiceTest {
 
         assertEquals(result, OPINION_DTO_LIST);
     }
+
+    @Test
+    @DisplayName("[ 2] given existing opinion id - when getOpinionById() - then return OpinionDTO")
+    public void givenExistingOpinionId_whenGetOpinionById_thenReturnOpinionDTO() {
+        String id = "633ca00a45ef711325f9d80f";
+
+        given(repository.findById(eq(id)))
+                .willReturn(Optional.of(OPINION));
+
+        OpinionDTO result = service.getOpinionById(id);
+
+        then(repository)
+                .should()
+                .findById(eq(id));
+
+        assertEquals(OPINION_DTO, result);
+    }
+
+    @Test
+    @DisplayName("[ 3] given not existing opinion id - when getOpinionById() then throw OpinionNotFoundException")
+    public void givenNotExistingOpinionId_whenGetOpinionById_thenThrowOpinionNotFoundException() {
+        String opinionId = "655ca00a45ef711325f9d80f";
+
+        given(repository.findById(eq(opinionId)))
+                .willThrow(new OpinionNotFoundException(opinionId));
+
+        assertThrows(
+                OpinionNotFoundException.class,
+                () -> service.getOpinionById(opinionId));
+    }
+
+    @Test
+    @DisplayName("[ 4] given existing opinions of user - when getOpinionByOpinionUserId() - then return List<OpinionDTO>")
+    public void givenExistingOpinionsOfUser_whenGetOpinionByOpinionUserId_thenReturnListOpinionDTO() {
+        String opinionUserId = "atyranski";
+
+        given(repository.findByOpinionUserId(eq(opinionUserId)))
+                .willReturn(OPINION_LIST);
+
+        List<OpinionDTO> result = service.getOpinionByOpinionUserId(opinionUserId);
+
+        then(repository)
+                .should()
+                .findByOpinionUserId(eq(opinionUserId));
+
+        assertEquals(OPINION_DTO_LIST, result);
+    }
+
+    @Test
+    @DisplayName("[ 5] given existing opinions for user - when getOpinionByReviewedUserId() - then return List<OpinionDTO>")
+    public void givenExistingOpinionsForUser_whenGetOpinionByReviewedUserId_thenReturnListOpinionDTO() {
+        String reviewedId = "pbogdan";
+        List<Opinion> opinions = List.of(OPINION);
+        List<OpinionDTO> opinionsDTO = OpinionMapper.toDtoList(opinions);
+
+        given(repository.findByReviewedUserId(eq(reviewedId)))
+                .willReturn(opinions);
+
+        List<OpinionDTO> result = service.getOpinionByReviewedUserId(reviewedId);
+
+        then(repository)
+                .should()
+                .findByReviewedUserId(eq(reviewedId));
+
+        assertEquals(opinionsDTO, result);
+    }
+
 }
