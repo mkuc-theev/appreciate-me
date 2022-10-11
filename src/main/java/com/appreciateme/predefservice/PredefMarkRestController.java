7cd7807ae783275cecf142007e6cd75baf211c96
Michał Kuć <mail.michael.kuc@gmail.com>
2022-10-11 11:28:47 +0200

    Changed URL formatting for HTTP requests

--- src/main/java/com/appreciateme/predefservice/PredefMarkRestController.java
+++ src/main/java/com/appreciateme/predefservice/PredefMarkRestController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,13 +32,13 @@ public class PredefMarkRestController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping(value = "/byId/{id}")
-    public PredefMarkDTO getPredefById(@PathVariable("id") String id) {
+    @GetMapping(value = "/byId")
+    public PredefMarkDTO getPredefById(@RequestParam(name = "id") String id) {
         return objectMapper.convertValue(predefMarkService.findByID(id).get(), PredefMarkDTO.class);
     }
 
-    @GetMapping(value = "/byName/{name}")
-    public PredefMarkDTO getPredefByName(@PathVariable("name") String name) {
+    @GetMapping(value = "/byName")
+    public PredefMarkDTO getPredefByName(@RequestParam(name = "name") String name) {
         return objectMapper.convertValue(predefMarkService.findByName(name).get(), PredefMarkDTO.class);
     }
 
@@ -48,8 +48,8 @@ public class PredefMarkRestController {
         return new ResponseEntity("Template added successfully.", HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/delete/{id}")
-    public ResponseEntity<?> deletePredefById(@PathVariable("id") String id) {
+    @DeleteMapping(value = "/deleteID")
+    public ResponseEntity<?> deletePredefById(@RequestParam(name = "id") String id) {
         predefMarkService.deletePredefByID(id);
         return new ResponseEntity("Template deleted successfully.", HttpStatus.OK);
     }
