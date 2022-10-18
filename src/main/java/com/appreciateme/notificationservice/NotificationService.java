dc46789a7ead9ab66b2b860e71cfa712815e0953
Michał Kuć <mail.michael.kuc@gmail.com>
2022-10-18 12:19:37 +0200

    fixed tests

--- src/main/java/com/appreciateme/notificationservice/NotificationService.java
+++ src/main/java/com/appreciateme/notificationservice/NotificationService.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 
 @SpringBootApplication
 public class NotificationService {
-
     public static void main(String[] args) {
         SpringApplication notificationService = new SpringApplication(NotificationService.class);
         notificationService.setDefaultProperties(Collections.singletonMap("server.port", "8004"));
