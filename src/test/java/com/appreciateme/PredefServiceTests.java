10c25e6e911d296270ff3c2d464ceb57f7ee14a5
Michał Kuć <mail.michael.kuc@gmail.com>
2022-10-18 11:49:07 +0200

    fixed tests again

--- src/test/java/com/appreciateme/PredefServiceTests.java
+++ src/test/java/com/appreciateme/PredefServiceTests.java
@@ -1,14 +1,9 @@
-package com.appreciateme.usersservice;
+package com.appreciateme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
-<<<<<<<< HEAD:src/test/java/com/appreciateme/PredefServiceTests.java
 class PredefServiceTests {
-========
-class UsersServiceApplicationTests {
->>>>>>>> origin/main:src/test/java/com/appreciateme/usersservice/UsersServiceApplicationTests.java
 
 	@Test
 	void contextLoads() {
