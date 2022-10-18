3b6e196b9df3cb123f930671192cbc327c661ae3
Michał Kuć <mail.michael.kuc@gmail.com>
2022-10-18 11:44:54 +0200

    fixed test file

--- src/test/java/com/appreciateme/usersservice/UsersServiceApplicationTests.java
+++ src/test/java/com/appreciateme/usersservice/UsersServiceApplicationTests.java
@@ -4,14 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<<< HEAD:src/test/java/com/appreciateme/PredefServiceTests.java
-class PredefServiceTests {
-========
 class UsersServiceApplicationTests {
->>>>>>>> origin/main:src/test/java/com/appreciateme/usersservice/UsersServiceApplicationTests.java
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
